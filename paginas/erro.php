634834e351dbacd70cba151e392dec5392d12018
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2025-05-30 12:25:58 -0300

    PaginaErroAjustado

--- paginas/erro.php
+++ paginas/erro.php
@@ -2,5 +2,6 @@
     <div class="container">
         <h2 class="text-center">A página que você está tentando acessar não existe!</h2>
         <img src="img/404.png" alt="404" class="text-center w-100">
+         <p class="text-center"><a href="home" class="especial">Retornar</a></p>
     </div>
 </main>
